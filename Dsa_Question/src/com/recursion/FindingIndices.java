23eefe99dacb909809224c89e82b2e0dd62a74a4
DebasishPati4 <debasishpati14@gmail.com>
2024-02-20 22:59:36 +0530

    SubSequence String is done

---
+++ Dsa_Question/src/com/recursion/FindingIndices.java
@@ -0,0 +1,30 @@
+package com.recursion;
+
+public class FindingIndices {
+    public static void main(String[] args) {
+
+        int[] fakeArr = { 21, 42, 15, 13, 4, 15, 15, 15 };
+
+        int[] foundIndices = findLastIndex(fakeArr, 15, fakeArr.length - 1, 0);
+        for (int i : foundIndices) {
+            System.out.print("LastIndex: " + i);
+        }
+    }
+
+    static int[] findLastIndex(int[] arr, int element, int index, int fsf) {
+        if (index == 0) {
+            int[] res = new int[fsf];
+            return res;
+        }
+        if (element == arr[index]) {
+            fsf++;
+        }
+        System.out.println(index);
+        int[] foundIndex = findLastIndex(arr, element, index - 1, fsf);
+        if (element == arr[index]) {
+            foundIndex[--fsf] = index;
+        }
+        return foundIndex;
+
+    }
+}
